01fa46997be5db24964da050b90334971a33ec81
Michael Vytlingam <michael.vytlingam@gmail.com>
2022-04-10 19:09:46 -0400

    remove image parsing and compare images instead

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from solver import solve
 
 def main():
     time.sleep(2)  # Time to open game window
+    pp = pprint.PrettyPrinter(indent=4)
     board = parse_board_from_screenshot()
+    
     score = 0
     trial_score = float('inf')
     while trial_score > 0:
         trial_score, board = solve(board)
         print(f'Trial: {trial_score}')
         score += trial_score
-    pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(board)
     print(f'Score: {score}')
 
--- parse_board.py
+++ parse_board.py
@@ -1,6 +1,8 @@
 import pyscreenshot as ImageGrab
-import pytesseract
 from constants import BOARD_TOP_BOUND, BOARD_LEFT_BOUND, NUMBER_HEIGHT, NUMBER_WIDTH, GAP_WIDTH, GAP_HEIGHT
+import numpy as np
+import os
+from PIL import Image
 
 
 def parse_board_from_screenshot():
@@ -15,8 +17,26 @@ def parse_board_from_screenshot():
         for j in range(17):
             right_bound = left_bound + NUMBER_WIDTH
             im = ImageGrab.grab(bbox=(left_bound, top_bound, right_bound, bottom_bound))
-            num = pytesseract.image_to_string(im, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=123456789')
+            num = get_number_from_image(im)
             board[i][j] = int(num)
             left_bound = right_bound + GAP_WIDTH
         top_bound = bottom_bound + GAP_HEIGHT
     return board
+
+
+def get_number_from_image(image):
+    for filename in os.listdir('number_images'):
+        f = os.path.join('number_images', filename)
+        known_image = Image.open(f)
+        if image_compare(image, known_image):
+            # Files are labelled by their number
+            return filename[0]
+
+
+def image_compare(image_1, image_2):
+    arr1 = np.array(image_1)
+    arr2 = np.array(image_2)
+    if arr1.shape != arr2.shape:
+        return False
+    maxdiff = np.max(np.abs(arr1 - arr2))
+    return maxdiff == 0
